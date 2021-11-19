f0f48c9f80b532ee51937467dae9766aeb867c0b
Nick Dobos <ninjoala@gmail.com>
2021-11-18 20:23:18 -0500

    Getting back to where I was

---
+++ mosley_website/Client/Pages/MosleyComponentBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace mosley_website.Client.Pages
+{
+    public class MosleyComponentBase : ComponentBase
+    {
+        [CascadingParameter]
+        public EventCallback PageLoad { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await PageLoad.InvokeAsync();
+            await base.OnInitializedAsync();
+        }
+    }
+}
