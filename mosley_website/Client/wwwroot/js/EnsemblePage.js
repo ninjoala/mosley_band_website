3aaa0c0ab212ad280b84c04fcdc23ccf152a98c0
Nick Dobos <ninjoala@gmail.com>
2022-01-15 13:25:28 -0500

    Locally scoped JS added to ensemble page

---
+++ mosley_website/Client/wwwroot/js/EnsemblePage.js
@@ -0,0 +1,13 @@
+﻿const scrollTop = document.getElementById('scrollTop');
+
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        scrollTop.style.display = "block";
+        scrollTop.style.opacity = 1;
+        console.log("block");
+    } else {
+        scrollTop.style.display = "none";
+        scrollTop.style.opacity = 0;
+        console.log("none");
+    }
+};
