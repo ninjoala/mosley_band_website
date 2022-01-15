f69fbbdff4a6d1967b40dfe0ebebdc7d87a1556b
Nick Dobos <ninjoala@gmail.com>
2022-01-15 12:40:34 -0500

    Scrolling to fragment even occurs when coming from same page

--- mosley_website/Client/Pages/MosleyComponentBase.cs
+++ mosley_website/Client/Pages/MosleyComponentBase.cs
@@ -6,8 +6,29 @@ using mosley_website.Client.Extensions;
 
 namespace mosley_website.Client.Pages
 {
-    public class MosleyComponentBase : ComponentBase
+    public class MosleyComponentBase : ComponentBase, IDisposable
     {
+        [Inject] NavigationManager NavManager { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
 
+        protected override void OnInitialized()
+        {
+            NavManager.LocationChanged += TryFragmentNavigation;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
+        {
+            await NavManager.NavigateToFragmentAsync(JsRuntime);
+        }
+
+        public void Dispose()
+        {
+            NavManager.LocationChanged -= TryFragmentNavigation;
+        }
     }
 }
