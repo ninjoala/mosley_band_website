94d386372271a526abcceb838f83c5ada2e58664
Nick Dobos <ninjoala@gmail.com>
2022-01-16 20:18:43 -0500

    Fixed navbar and dropdowns and menu button

--- mosley_website/Client/wwwroot/js/NavMenu.js
+++ mosley_website/Client/wwwroot/js/NavMenu.js
@@ -1,10 +1,10 @@
 ﻿const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const hamburgerLines = document.querySelector(".hamburger-lines");
-
+const navLinks = document.getElementsByClassName("nav-link");
 
 navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible')
+    const visibility = primaryNav.getAttribute('data-visible');
 
     if (visibility === "false") {
         primaryNav.setAttribute('data-visible', true);
@@ -16,4 +16,18 @@ navToggle.addEventListener('click', () => {
         console.log("false");
     }
 })
+
+function closeMobileNav() {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        hamburgerLines.setAttribute("data-transformed", false)
+        console.log("false");
+    }
+}
+
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', closeMobileNav, false);
+}
 
